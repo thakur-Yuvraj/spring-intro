bb4a9c7c9cb986ab00d4b1d2c12cea0bf35ed806
thakur-Yuvraj <yuvipatel224@gmail.com>
2025-02-22 10:30:26 +0530

    [Yuvraj] Add. Hello Mark from bridgeLabz

--- SpringIntroduction/src/main/java/com/restapi/controller/AppController.java
+++ SpringIntroduction/src/main/java/com/restapi/controller/AppController.java
@@ -1,17 +1,24 @@
 package com.restapi.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.GetMapping;
-@RestController
-@RequestMapping("/hello")
+
 
 // controller class
+@RestController
+@RequestMapping("/hello")
 public class AppController {
 
     // mapping is now on /hello
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
